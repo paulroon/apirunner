34e1129aedfa0add14acf5c714e9da642e543573
Paul Rooney <paulroon@gmail.com>
2014-09-30 15:54:16 +0100

    Oauth2 tweaks

--- run.php
+++ run.php
@@ -4,6 +4,12 @@ include __DIR__ . '/vendor/autoload.php';
 use HappyCode\Core\ApiRunner;
 use HappyCode\Core\Boot;
 
-$apiRunner = new ApiRunner(Boot::Load("config/endpoints.yml"));
+$token = isset($argv[1]) ? $argv[1] : false;
+
+$apiRunner = new ApiRunner(Boot::Load("config/endpoints.yml"), $token);
+
 $apiRunner->run()->report();
+
+
+
 
--- src/Core/ApiRunner.php
+++ src/Core/ApiRunner.php
@@ -9,60 +9,83 @@ class ApiRunner {
     protected $endpoints;
 
     protected $client;
-    protected $report;
+    protected $report = [];
 
-    public function __construct($configs) {
+    public function __construct($configs, $token = false) {
 
         if(isset($configs['endpoints'])) {
             $this->endpoints = $configs['endpoints'];
         }
-        
+
         $protocol = $configs['api']['use_ssl'] ? 'https' : 'http';
+        $authenticationToken = (!!$token) ? $token : $configs['api']['auth_token'];
         $this->client = new Client([
             'base_url' => $protocol . '://' . $configs['api']['base_url'],
             'defaults' => [
                 'query' => $configs['api']['default']['query'],
                 'headers' => [
-                    'Authorization' => 'Bearer ' . $configs['api']['auth_token'],
+                    'Authorization' => 'Bearer ' . $authenticationToken,
                     'Cookie' => $configs['api']['default']['cookie']
                 ]
             ]
         ]);
 
+        $this->checkAuth($configs['api']['oauth2']['verify_uri']);
+
+    }
+
+    /**
+     * override the token from the configs
+     * @param $token
+     */
+    public function setAuthToken($token) {
+        $this->client->setDefaultOption('headers/Authorization', 'Bearer ' . $token);
+    }
+
+
+    private function checkAuth($verify_uri){
+
+        echo "Using Token " . $this->client->getDefaultOption('headers/Authorization');
+
+        $st = microtime(true);
+        try{
+            $response = $this->client->get($verify_uri, []);
+
+            $user = $response->json();
+
+            $this->reportSuccess([
+                "name" => "Token Authentication",
+                "path" => $verify_uri
+            ], $response, (microtime(true) - $st));
+
+            echo PHP_EOL . "Token Authenticated." . ", Hi " . $user['username'] . PHP_EOL;
+        }catch(\Exception $e){
+            echo PHP_EOL . "Oauth Authentication Failed in " . number_format((microtime(true) - $st), 3) . " secs" . PHP_EOL . "   - " . $e->getMessage() . PHP_EOL;
+            exit;
+        }
+        return false;
+
     }
 
     public function run(){
 
-        $this->report = [];
         /**
          * Guzzle is very capable of sending requests in parallel - however this is a performance tool as well as a
          * for testing purposes
          * I don't want the requests to interfere with each other (or do I)?
          * so have chosen to do a (slower)? sequential set of tests
          */
-        foreach($this->endpoints as $apicall){
+        foreach($this->endpoints as $apiCall){
 
-            echo PHP_EOL . "running " . $apicall['name'] . "...";
+            echo PHP_EOL . "running " . $apiCall['name'] . "...";
 
             try{
                 $start = microtime(true);
-                $res = $this->doRequest($apicall);
-                $took = microtime(true) - $start;
-                $this->report[] = [
-                    'success' => true,
-                    'name' => $apicall['name'],
-                    'time' => number_format($took, 3),
-                    'status_code' => $res->getStatusCode(),
-                    'content_type' => $res->getHeader('content-type'),
-                    'response_length' => strlen($res->getBody())
-                ];
+                $res = $this->doRequest($apiCall);
+                $this->reportSuccess($apiCall, $res, (microtime(true) - $start));
             }catch(\Exception $e){
                 echo '['.get_class($e).'] - ' . $e->getMessage() . PHP_EOL;
-                $this->report[] = [
-                    'success' => false,
-                    'name' => $apicall['name'],
-                    'error' => $e->getMessage()
-                ];
+                $this->reportFail($apiCall, $e);
             }
 
         }
@@ -73,27 +96,51 @@ class ApiRunner {
     public function doRequest($apiCall) {
 
         $options = [];
-        foreach(['query', 'body'] as $opt) {
+        foreach(['query', 'body', 'headers'] as $opt) {
             if(isset($apiCall[$opt])) {
-                if($opt == "query"){
-                    var_export($apiCall[$opt]);
+
+                // Endpoint query arrays are either augmented to the default OR et totally empty
+                if(is_array($apiCall[$opt]) && empty($apiCall[$opt])){
+                    $this->client->setDefaultOption($opt, []);
                 }
+
+                // Redefine
+                if($opt == "headers"){
+                    $this->client->setDefaultOption($opt, []);
+                }
+
+
                 $options[$opt] = $apiCall[$opt];
             }
         }
 
-        switch(strtoupper($apiCall['method'])) {
+        $request = $this->client->createRequest(strtoupper($apiCall['method']), $apiCall['path'], $options);
 
-            case 'POST':        $response =  $this->client->post($apiCall['path'], $options); break;
-            case 'PUT':         $response =  $this->client->put($apiCall['path'], $options); break;
-            case 'DELETE':      $response =  $this->client->delete($apiCall['path'], $options); break;
-            case 'GET':
-            default:
-                                $response = $this->client->get($apiCall['path'], ['query' => $options]); break;
-        }
+        $response = $this->client->send($request);
+
+//        $responseBody = $response->json();
 
         return $response;
 
+    }
+
+    public function reportSuccess($apiCall, $res, $took){
+        $this->report[] = [
+            'success' => true,
+            'name' => $apiCall['name'],
+            'time' => number_format($took, 3),
+            'status_code' => $res->getStatusCode(),
+            'content_type' => $res->getHeader('content-type'),
+            'response_length' => strlen($res->getBody())
+        ];
+    }
+
+    public function reportFail($apiCall, $e){
+        $this->report[] = [
+            'success' => false,
+            'name' => $apiCall['name'],
+            'error' => $e->getMessage()
+        ];
     }
 
     public function report(){
@@ -123,4 +170,4 @@ class ApiRunner {
         }
         echo $resultRow();
     }
-} 
+}
