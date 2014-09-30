1c727f1eb76dc79a894eb9591cb03f1125851514
Paul Rooney <paulroon@gmail.com>
2014-09-30 11:37:19 +0100

    global request defaults and array syntax definitions for query parameters

--- run.php
+++ run.php
@@ -5,30 +5,5 @@ use HappyCode\Core\ApiRunner;
 use HappyCode\Core\Boot;
 
 $apiRunner = new ApiRunner(Boot::Load("config/endpoints.yml"));
-$report = $apiRunner->run();
+$apiRunner->run()->report();
 
-echo PHP_EOL . PHP_EOL . "====== REPORT ====== " . PHP_EOL . PHP_EOL;
-
-
-$resultRow = function($name = null, $time = null, $st = null, $r_len = null, $ct = null){
-    $pad = ($name == null) ? "+" : " ";
-    return sprintf("+ %s + %s + %s + %s + %s +" .  PHP_EOL,
-        str_pad($name, 20, $pad, STR_PAD_RIGHT),
-        str_pad($time, 15, $pad, STR_PAD_BOTH),
-        str_pad($st, 10, $pad, STR_PAD_BOTH),
-        str_pad($r_len, 20, $pad, STR_PAD_BOTH),
-        str_pad($ct, 20, $pad, STR_PAD_RIGHT)
-    );
-};
-
-echo $resultRow();
-echo $resultRow("Endpoint", "(Time )secs", "Status", "Content-Length", "Type");
-echo $resultRow();
-foreach($report as $callReport) {
-    if($callReport['success']){
-        echo $resultRow($callReport['name'], $callReport['time'], $callReport['status_code'], $callReport['response_length'], $callReport['content_type']);
-    }else{
-        echo sprintf("+ %s: ERROR: (%s) +" .  PHP_EOL, str_pad($callReport['name'], 20, " ", STR_PAD_RIGHT), $callReport['error']);
-    }
-}
-echo $resultRow();
--- src/Core/ApiRunner.php
+++ src/Core/ApiRunner.php
@@ -9,20 +9,22 @@ class ApiRunner {
     protected $endpoints;
 
     protected $client;
+    protected $report;
 
     public function __construct($configs) {
 
-        if(isset($configs['endpoints'])){
+        if(isset($configs['endpoints'])) {
             $this->endpoints = $configs['endpoints'];
         }
-
+        
         $protocol = $configs['api']['use_ssl'] ? 'https' : 'http';
         $this->client = new Client([
             'base_url' => $protocol . '://' . $configs['api']['base_url'],
             'defaults' => [
+                'query' => $configs['api']['default']['query'],
                 'headers' => [
                     'Authorization' => 'Bearer ' . $configs['api']['auth_token'],
-                    'Cookie' => 'XDEBUG_SESSION=PHPSTORM'
+                    'Cookie' => $configs['api']['default']['cookie']
                 ]
             ]
         ]);
@@ -31,9 +33,9 @@ class ApiRunner {
 
     public function run(){
 
-        $report = [];
+        $this->report = [];
         /**
-         * Guzzle is very capable of sending requests in paralel - however this is a performance tool as well as a
+         * Guzzle is very capable of sending requests in parallel - however this is a performance tool as well as a
          * for testing purposes
          * I don't want the requests to interfere with each other (or do I)?
          * so have chosen to do a (slower)? sequential set of tests
@@ -46,7 +48,7 @@ class ApiRunner {
                 $start = microtime(true);
                 $res = $this->doRequest($apicall);
                 $took = microtime(true) - $start;
-                $report[] = [
+                $this->report[] = [
                     'success' => true,
                     'name' => $apicall['name'],
                     'time' => number_format($took, 3),
@@ -56,7 +58,7 @@ class ApiRunner {
                 ];
             }catch(\Exception $e){
                 echo '['.get_class($e).'] - ' . $e->getMessage() . PHP_EOL;
-                $report[] = [
+                $this->report[] = [
                     'success' => false,
                     'name' => $apicall['name'],
                     'error' => $e->getMessage()
@@ -65,17 +67,60 @@ class ApiRunner {
 
         }
 
-        return $report;
+        return $this;
     }
 
-    public function doRequest($apicall) {
+    public function doRequest($apiCall) {
 
-        switch(strtoupper($apicall['method'])){
-            case 'POST': $response =  $this->client->post($apicall['path'], ['query' => parse_str($apicall['query'])]); break;
-            case 'GET':
-            default: $response = $this->client->get($apicall['path'], ['query' => parse_str($apicall['query'])]); break;
+        $options = [];
+        foreach(['query', 'body'] as $opt) {
+            if(isset($apiCall[$opt])) {
+                if($opt == "query"){
+                    var_export($apiCall[$opt]);
+                }
+                $options[$opt] = $apiCall[$opt];
+            }
         }
+
+        switch(strtoupper($apiCall['method'])) {
+
+            case 'POST':        $response =  $this->client->post($apiCall['path'], $options); break;
+            case 'PUT':         $response =  $this->client->put($apiCall['path'], $options); break;
+            case 'DELETE':      $response =  $this->client->delete($apiCall['path'], $options); break;
+            case 'GET':
+            default:
+                                $response = $this->client->get($apiCall['path'], ['query' => $options]); break;
+        }
+
         return $response;
 
+    }
+
+    public function report(){
+        echo PHP_EOL . PHP_EOL . "====== REPORT ====== " . PHP_EOL . PHP_EOL;
+
+
+        $resultRow = function($name = null, $time = null, $st = null, $r_len = null, $ct = null){
+            $pad = ($name == null) ? "+" : " ";
+            return sprintf("+ %s + %s + %s + %s + %s +" .  PHP_EOL,
+                str_pad($name, 20, $pad, STR_PAD_RIGHT),
+                str_pad($time, 15, $pad, STR_PAD_BOTH),
+                str_pad($st, 10, $pad, STR_PAD_BOTH),
+                str_pad($r_len, 20, $pad, STR_PAD_BOTH),
+                str_pad($ct, 20, $pad, STR_PAD_RIGHT)
+            );
+        };
+
+        echo $resultRow();
+        echo $resultRow("Endpoint", "(Time )secs", "Status", "Content-Length", "Type");
+        echo $resultRow();
+        foreach($this->report as $callReport) {
+            if($callReport['success']){
+                echo $resultRow($callReport['name'], $callReport['time'], $callReport['status_code'], $callReport['response_length'], $callReport['content_type']);
+            }else{
+                echo sprintf("+ %s: ERROR: (%s) +" .  PHP_EOL, str_pad($callReport['name'], 20, " ", STR_PAD_RIGHT), $callReport['error']);
+            }
+        }
+        echo $resultRow();
     }
 } 
